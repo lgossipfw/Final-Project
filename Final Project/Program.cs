dac1b78e01309b22b84d3b9ebb18903a6cc65c28
lgoss <gosslm01@students.ipfw.edu>
2017-04-26 14:10:13 -0400

    Chanaged startup form

--- Final Project/Program.cs
+++ Final Project/Program.cs
@@ -16,7 +16,7 @@ namespace Final_Project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CustomerForm());
+            Application.Run(new LoginForm());
         }
     }
 }
